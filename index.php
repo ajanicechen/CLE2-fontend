a2ecf0d844796a2289f35f61c4bf6ad5a0a6bbe2
Anniee <73761657+ajanicechen@users.noreply.github.com>
2021-01-28 21:32:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,49 @@
 <head>
     <meta charset="UTF-8">
     <title>Hakizen's Commissions!</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>HA • KI • ZEN</h1>
+    <nav>
+        <a href="#home">Home</a>
+        <a href="commissionStyles.php"> Styles</a>
+        <a href="form.php">Form</a>
+        <a href="#socials">Social Media</a>
+    </nav>
+    <section id="home">
+      <p>
+      <h1>HA • KI • ZEN</h1>
+      <h2>Commission Info</h2>
+      </p>
+      <div class="rules">
+        <p>
+                - Please describe or link a picture of the character/person you want me to draw! You could also send pictures of character/person to me on Twitter/Insta!
+            </p>
+            <p>
+                - If you want me to draw them in a specific pose, let me know! Please link a picture or a quick sketch of the pose in the (doesn't matter if it's 'bad'). If you don't have a picture or sketch, please describe it as best as you can.
+            </p>
+            <p>
+                - Let me know what color background you want
+            </p>
+        </div>
+        <div class="rules">
+            <p>
+                Terms of services
+            </p>
+            <p>
+                I will message you when I start working on your commission.
+                Because of school I may take a maximum of 2 weeks on each commission.
+                Please keep this in mind before commissioning.
+            </p>
+        </div>
+        <div class="links">
+            <a href="commissionStyles.php">Commission Styles</a>
+            <a href="form.php">Commission Here!</a>
+        </div>
+    </section>
 </body>
+<footer id="socials">
+    <a href="https://twitter.com/hakizenk">Twitter</a>
+    <a href="https://www.instagram.com/hakizen/">Instagram</a>
+    <a href="https://ko-fi.com/hakizen">Ko-fi</a>
+</footer>
