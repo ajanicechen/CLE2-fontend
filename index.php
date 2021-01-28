86027bceb04513419c791761942b336af5b78839
Anniee <73761657+ajanicechen@users.noreply.github.com>
2021-01-28 18:36:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hakizen's Commissions!</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h1>HA • KI • ZEN</h1>
+</body>
